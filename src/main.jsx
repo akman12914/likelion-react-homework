c183cc30674b8c71d066b070e886875070bf797c
akman12914 <rnlduadns98@naver.com>
2025-01-19 18:11:05 +0900

    [구성] JSX사용, App 컴포넌트 작성

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/main.css";
+import App from "./app";
+
+const root = document.getElementById("root");
+
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
