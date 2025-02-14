c3b10b5ddd3960453f0256d6bbe159db21a7cc64
akman12914 <rnlduadns98@naver.com>
2025-02-15 03:11:15 +0900

    [구성] 폴더 구성

--- src/app.tsx
+++
@@ -1,6 +0,0 @@
-import Cart from './page/cartPage';
-function App() {
-  return <Cart />;
-}
-
-export default App;
--- src/types/cartItemtype.ts
+++
@@ -1,7 +0,0 @@
-export interface CartItemType {
-  id: number;
-  name: string;
-  price: number;
-  image: string;
-  quantity: number;
-}
